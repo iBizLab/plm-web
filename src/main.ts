4b84e0228b4ba31d083df8a19af4306176d642fb
zhangpingchuan <228939628@qq.com>
2025-06-10 14:39:37 +0800

    feat:  更新模板静态内容 -f1

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import 'vue';
+// import { IApiAppHubService } from '@ibiz-template/runtime';
+// import { AppHooks } from '@ibiz-template/vue3-util';
 import { runApp } from '@ibiz-template/vue3-components';
 import VueTextFormat from 'vue-text-format';
 import gridLayout from 'vue-grid-layout';
@@ -7,6 +9,15 @@ import ComponentsRegister from './components/index';
 import { StaticAuthGuard } from './guard/auth-guard/auth-guard';
 import '@/publish/sys.css';
 
+// AppHooks.appResorceInited.tap((ctx: IApiAppHubService) => {
+//   ctx.microAppConfigCenter.registerMicroApps([
+//     {
+//       name: 'plmcomweb',
+//       entry: 'http://localhost:4173',
+//       baseUrl: '4178bef3e0e0f43479aea070a3ee4188__plmweb__plmcomweb',
+//     },
+//   ]);
+// });
 runApp([VueTextFormat, gridLayout, ComponentsRegister, UserRegister], {
   getAuthGuard: () => new StaticAuthGuard(),
 });
